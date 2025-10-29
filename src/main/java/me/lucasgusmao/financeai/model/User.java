769041f28a81ffa16cc22d47649250487dc061f8
Lucas Gusmão <130295673+lucasgusmaobaptista@users.noreply.github.com>
2025-10-29 18:20:57 -0300

    feat(user): create User entity.
    Persist tables and columns
    Add Pre-persist and Pre-update methods
    Add NoArgsConstructor and Default constructor

---
+++ src/main/java/me/lucasgusmao/financeai/model/User.java
@@ -0,0 +1,48 @@
+package me.lucasgusmao.financeai.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "username", unique = true)
+    private String username;
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public User(String name, String email, String username, String password) {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+    
+}
