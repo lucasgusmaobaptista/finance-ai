3517cc34b9f6ae49b4f642f55f7921d9a74e7000
Lucas Gusmão <130295673+lucasgusmaobaptista@users.noreply.github.com>
2025-10-29 18:19:00 -0300

    add and fix some dependecies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,7 +8,34 @@ module me.lucasgusmao.financeai {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires jakarta.persistence;
+    requires static lombok;
+    requires spring.context;
+    requires spring.data.jpa;
+    requires spring.security.crypto;
+    requires spring.web;
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+    requires spring.beans;
+    requires spring.core;
 
-    opens me.lucasgusmao.financeai to javafx.fxml;
+    requires org.hibernate.orm.core;
+    requires java.sql;
+    requires static java.instrument;
+
+    opens me.lucasgusmao.financeai to javafx.fxml, spring.core, spring.beans, spring.context;
+    opens me.lucasgusmao.financeai.config to spring.core, spring.beans, spring.context;
+    opens me.lucasgusmao.financeai.model;
+    opens me.lucasgusmao.financeai.service to spring.core, spring.beans, spring.context;
+    opens me.lucasgusmao.financeai.repository to spring.core, spring.beans, spring.context;
+    opens me.lucasgusmao.financeai.controller to javafx.fxml, spring.core, spring.beans, spring.context;
+    opens me.lucasgusmao.financeai.view to javafx.fxml;
+
+    exports me.lucasgusmao.financeai.view;
+    exports me.lucasgusmao.financeai.controller;
+    exports me.lucasgusmao.financeai.config;
     exports me.lucasgusmao.financeai;
+    exports me.lucasgusmao.financeai.model;
+    exports me.lucasgusmao.financeai.service;
+    exports me.lucasgusmao.financeai.repository;
 }
