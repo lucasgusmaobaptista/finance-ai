0475fb2fa811a027c6076fc38d1ec1c0f389c2fd
lucasgusmaobaptista <lucasgusmaobaptista@gmail.com>
2025-11-16 11:06:43 -0300

    update(database): update application.yml connection with database.

--- src/main/java/me/lucasgusmao/financeai/Application.java
+++ src/main/java/me/lucasgusmao/financeai/Application.java
@@ -2,6 +2,6 @@ package me.lucasgusmao.financeai;
 
 public class Application {
     public static void main(String[] args) {
-        javafx.application.Application.launch(HelloApplication.class, args);
+        javafx.application.Application.launch(FinanceApplication.class, args);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -23,20 +23,21 @@ module me.lucasgusmao.financeai {
     requires org.hibernate.orm.core;
     requires java.sql;
     requires static java.instrument;
+    requires spring.tx;
 
     opens me.lucasgusmao.financeai to javafx.fxml, spring.core, spring.beans, spring.context;
     opens me.lucasgusmao.financeai.config to spring.core, spring.beans, spring.context;
-    opens me.lucasgusmao.financeai.model;
+    opens me.lucasgusmao.financeai.model.entity;
     opens me.lucasgusmao.financeai.service to spring.core, spring.beans, spring.context;
     opens me.lucasgusmao.financeai.repository to spring.core, spring.beans, spring.context;
     opens me.lucasgusmao.financeai.controller to javafx.fxml, spring.core, spring.beans, spring.context;
-    opens me.lucasgusmao.financeai.view to javafx.fxml;
+    opens me.lucasgusmao.financeai.screens to javafx.fxml;
 
-    exports me.lucasgusmao.financeai.view;
+    exports me.lucasgusmao.financeai.screens;
     exports me.lucasgusmao.financeai.controller;
     exports me.lucasgusmao.financeai.config;
     exports me.lucasgusmao.financeai;
-    exports me.lucasgusmao.financeai.model;
+    exports me.lucasgusmao.financeai.model.entity;
     exports me.lucasgusmao.financeai.service;
     exports me.lucasgusmao.financeai.repository;
 }
