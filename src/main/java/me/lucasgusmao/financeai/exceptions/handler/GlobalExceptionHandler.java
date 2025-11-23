b580ae8d9fe501af35581a5185e6c7e5ac796863
lucasgusmaobaptista <lucasgusmaobaptista@gmail.com>
2025-11-23 17:46:42 -0300

    implement exception handler for invalid operation exception

--- src/main/java/me/lucasgusmao/financeai/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/me/lucasgusmao/financeai/exceptions/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package me.lucasgusmao.financeai.exceptions.handler;
 
 import me.lucasgusmao.financeai.exceptions.custom.AlreadyExistsException;
+import me.lucasgusmao.financeai.exceptions.custom.InvalidOperationException;
 import me.lucasgusmao.financeai.exceptions.response.ResponseError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -8,6 +9,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(AlreadyExistsException.class)
     public ResponseError handleAlreadyExists(AlreadyExistsException e) {
+        return ResponseError.defaultResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(InvalidOperationException.class)
+    public ResponseError handleInvalidOperation(InvalidOperationException e) {
         return ResponseError.defaultResponse(e.getMessage());
     }
 }
