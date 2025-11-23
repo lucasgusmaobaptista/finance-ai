fd541b394df9b0da14b3b7699c4976daf1db6096
lucasgusmaobaptista <lucasgusmaobaptista@gmail.com>
2025-11-23 17:46:01 -0300

    create invalidoperationexception

---
+++ src/main/java/me/lucasgusmao/financeai/exceptions/custom/InvalidOperationException.java
@@ -0,0 +1,7 @@
+package me.lucasgusmao.financeai.exceptions.custom;
+
+public class InvalidOperationException extends RuntimeException {
+    public InvalidOperationException(String message) {
+        super(message);
+    }
+}
